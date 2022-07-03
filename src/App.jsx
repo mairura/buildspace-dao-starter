8fed83f7704de77d4d24e916414a170fe2aecb0f
ted <mairurat@gmail.com>
2022-07-03 12:17:28 +0300

    cheching for networks available

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const memberList = useMemo(() => {
 if (hasClaimedNFT) {
   return (
     <div className="member-page">
-      <h1>🍪DAO Member Page</h1>
+      <h1>🍪ArticleDAO Member Page</h1>
       <p>Congratulations on being a member</p>
       <div>
         <div>
